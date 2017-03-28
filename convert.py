f9536664c6f50212d0ecc39a6bf9cf07b04c6b5a
dsindex <hazzling@gmail.com>
2017-03-28 21:05:16 +0900

    skip segmentation spans

--- convert.py
+++ convert.py
@@ -11,37 +11,38 @@ VERBOSE = 0
 
 if __name__ == '__main__':
 
-	parser = OptionParser()
-	parser.add_option("--verbose", action="store_const", const=1, dest="verbose", help="verbose mode")
-	(options, args) = parser.parse_args()
-	
-	if options.verbose == 1 : VERBOSE = 1
+    parser = OptionParser()
+    parser.add_option("--verbose", action="store_const", const=1, dest="verbose", help="verbose mode")
+    (options, args) = parser.parse_args()
+    
+    if options.verbose == 1 : VERBOSE = 1
 
-	startTime = time.time()
+    startTime = time.time()
 
-	data = {}
+    data = {}
 
-	while 1 :
-		try : line = sys.stdin.readline()
-		except KeyboardInterrupt : break
-		if not line : break
+    while 1 :
+        try : line = sys.stdin.readline()
+        except KeyboardInterrupt : break
+        if not line : break
 
-		line = line.strip()
-		if not line :
-			print ''
-			continue
-		if line[0] == '#' :
-			print line
-			continue
+        line = line.strip()
+        if not line :
+            print ''
+            continue
+        if line[0] == '#' :
+            print line
+            continue
 
-		tokens = line.split('\t')
-		id = tokens[0]
-		if '.' in id : continue   # ex) 8.1
-		if tokens[4] == '_' :
-			tokens[4] = tokens[3] # there is no XPOS
-		else :
-			tokens[3] = tokens[4] # UPOS <- XPOS
-		print '\t'.join(tokens)
+        tokens = line.split('\t')
+        id = tokens[0]
+        # skip empty node spans and segmentation spans, ex) 8.1, 2-4
+        if '.' in id or '-' in id : continue
+        if tokens[4] == '_' :
+            tokens[4] = tokens[3] # there is no XPOS, XPOS <- UPOS
+        else :
+            tokens[3] = tokens[4] # UPOS <- XPOS
+        print '\t'.join(tokens)
 
-	durationTime = time.time() - startTime
-	sys.stderr.write("duration time = %f\n" % durationTime)
+    durationTime = time.time() - startTime
+    sys.stderr.write("duration time = %f\n" % durationTime)

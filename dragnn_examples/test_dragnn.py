2d84c311544e626280931cb651ac97365b77d8f9
dsindex <hazzling@gmail.com>
2017-03-28 09:52:28 +0900

    -

--- dragnn_examples/test_dragnn.py
+++ dragnn_examples/test_dragnn.py
@@ -54,99 +54,99 @@ BATCH_SIZE = FLAGS.batch_size
 
 
 def build_master_spec() :
-  '''
-  # Left-to-right, character-based LSTM.
-  char2word = spec_builder.ComponentSpecBuilder('char_lstm')
-  char2word.set_network_unit(
-      name='wrapped_units.LayerNormBasicLSTMNetwork',
-      hidden_layer_sizes='256')
-  char2word.set_transition_system(name='char-shift-only', left_to_right='true')
-  char2word.add_fixed_feature(name='chars', fml='char-input.text-char',
-                              embedding_dim=16)
-  char2word.fill_from_resources(DATA_DIR)
+    '''
+    # Left-to-right, character-based LSTM.
+    char2word = spec_builder.ComponentSpecBuilder('char_lstm')
+    char2word.set_network_unit(
+        name='wrapped_units.LayerNormBasicLSTMNetwork',
+        hidden_layer_sizes='256')
+    char2word.set_transition_system(name='char-shift-only', left_to_right='true')
+    char2word.add_fixed_feature(name='chars', fml='char-input.text-char',
+                                embedding_dim=16)
+    char2word.fill_from_resources(DATA_DIR)
 
-  # Lookahead LSTM reads right-to-left to represent the rightmost context of the
-  # words. It gets word embeddings from the char model.
-  lookahead = spec_builder.ComponentSpecBuilder('lookahead')
-  lookahead.set_network_unit(
-      name='wrapped_units.LayerNormBasicLSTMNetwork',
-      hidden_layer_sizes='256')
-  lookahead.set_transition_system(name='shift-only', left_to_right='false')
-  lookahead.add_link(source=char2word, fml='input.last-char-focus',
-                     embedding_dim=64)
-  lookahead.fill_from_resources(DATA_DIR)
-  '''
+    # Lookahead LSTM reads right-to-left to represent the rightmost context of the
+    # words. It gets word embeddings from the char model.
+    lookahead = spec_builder.ComponentSpecBuilder('lookahead')
+    lookahead.set_network_unit(
+        name='wrapped_units.LayerNormBasicLSTMNetwork',
+        hidden_layer_sizes='256')
+    lookahead.set_transition_system(name='shift-only', left_to_right='false')
+    lookahead.add_link(source=char2word, fml='input.last-char-focus',
+                       embedding_dim=64)
+    lookahead.fill_from_resources(DATA_DIR)
+    '''
 
-  # Construct the 'lookahead' ComponentSpec. This is a simple right-to-left RNN
-  # sequence model, which encodes the context to the right of each token. It has
-  # no loss except for the downstream components.
-  lookahead = spec_builder.ComponentSpecBuilder('lookahead')
-  lookahead.set_network_unit(
-      name='FeedForwardNetwork', hidden_layer_sizes='256')
-  lookahead.set_transition_system(name='shift-only', left_to_right='true')
-  lookahead.add_fixed_feature(name='words', fml='input.word', embedding_dim=64)
-  lookahead.add_rnn_link(embedding_dim=-1)
-  lookahead.fill_from_resources(DATA_DIR)
+    # Construct the 'lookahead' ComponentSpec. This is a simple right-to-left RNN
+    # sequence model, which encodes the context to the right of each token. It has
+    # no loss except for the downstream components.
+    lookahead = spec_builder.ComponentSpecBuilder('lookahead')
+    lookahead.set_network_unit(
+        name='FeedForwardNetwork', hidden_layer_sizes='256')
+    lookahead.set_transition_system(name='shift-only', left_to_right='true')
+    lookahead.add_fixed_feature(name='words', fml='input.word', embedding_dim=64)
+    lookahead.add_rnn_link(embedding_dim=-1)
+    lookahead.fill_from_resources(DATA_DIR)
 
-  # Construct the tagger. This is a simple left-to-right LSTM sequence tagger.
-  tagger = spec_builder.ComponentSpecBuilder('tagger')
-  tagger.set_network_unit(
-      name='wrapped_units.LayerNormBasicLSTMNetwork',
-      hidden_layer_sizes='256')
-  tagger.set_transition_system(name='tagger')
-  tagger.add_token_link(source=lookahead, fml='input.focus', embedding_dim=64)
-  tagger.fill_from_resources(DATA_DIR)
+    # Construct the tagger. This is a simple left-to-right LSTM sequence tagger.
+    tagger = spec_builder.ComponentSpecBuilder('tagger')
+    tagger.set_network_unit(
+        name='wrapped_units.LayerNormBasicLSTMNetwork',
+        hidden_layer_sizes='256')
+    tagger.set_transition_system(name='tagger')
+    tagger.add_token_link(source=lookahead, fml='input.focus', embedding_dim=64)
+    tagger.fill_from_resources(DATA_DIR)
 
-  # Construct the parser.
-  parser = spec_builder.ComponentSpecBuilder('parser')
-  parser.set_network_unit(name='FeedForwardNetwork', hidden_layer_sizes='256',
-                          layer_norm_hidden='true')
-  parser.set_transition_system(name='arc-standard')
-  parser.add_token_link(source=lookahead, fml='input.focus', embedding_dim=64)
-  parser.add_token_link(
-      source=tagger, fml='input.focus stack.focus stack(1).focus',
-      embedding_dim=64)
+    # Construct the parser.
+    parser = spec_builder.ComponentSpecBuilder('parser')
+    parser.set_network_unit(name='FeedForwardNetwork', hidden_layer_sizes='256',
+                            layer_norm_hidden='true')
+    parser.set_transition_system(name='arc-standard')
+    parser.add_token_link(source=lookahead, fml='input.focus', embedding_dim=64)
+    parser.add_token_link(
+        source=tagger, fml='input.focus stack.focus stack(1).focus',
+        embedding_dim=64)
 
-  # Add discrete features of the predicted parse tree so far, like in Parsey
-  # McParseface.
-  parser.add_fixed_feature(name='labels', embedding_dim=16,
-                           fml=' '.join([
-                               'stack.child(1).label',
-                               'stack.child(1).sibling(-1).label',
-                               'stack.child(-1).label',
-                               'stack.child(-1).sibling(1).label',
-                               'stack(1).child(1).label',
-                               'stack(1).child(1).sibling(-1).label',
-                               'stack(1).child(-1).label',
-                               'stack(1).child(-1).sibling(1).label',
-                               'stack.child(2).label',
-                               'stack.child(-2).label',
-                               'stack(1).child(2).label',
-                               'stack(1).child(-2).label']))
+    # Add discrete features of the predicted parse tree so far, like in Parsey
+    # McParseface.
+    parser.add_fixed_feature(name='labels', embedding_dim=16,
+                             fml=' '.join([
+                                 'stack.child(1).label',
+                                 'stack.child(1).sibling(-1).label',
+                                 'stack.child(-1).label',
+                                 'stack.child(-1).sibling(1).label',
+                                 'stack(1).child(1).label',
+                                 'stack(1).child(1).sibling(-1).label',
+                                 'stack(1).child(-1).label',
+                                 'stack(1).child(-1).sibling(1).label',
+                                 'stack.child(2).label',
+                                 'stack.child(-2).label',
+                                 'stack(1).child(2).label',
+                                 'stack(1).child(-2).label']))
 
-  # Recurrent connection for the arc-standard parser. For both tokens on the
-  # stack, we connect to the last time step to either SHIFT or REDUCE that
-  # token. This allows the parser to build up compositional representations of
-  # phrases.
-  parser.add_link(
-      source=parser,  # recurrent connection
-      name='rnn-stack',  # unique identifier
-      fml='stack.focus stack(1).focus',  # look for both stack tokens
-      source_translator='shift-reduce-step',  # maps token indices -> step
-      embedding_dim=64)  # project down to 64 dims
+    # Recurrent connection for the arc-standard parser. For both tokens on the
+    # stack, we connect to the last time step to either SHIFT or REDUCE that
+    # token. This allows the parser to build up compositional representations of
+    # phrases.
+    parser.add_link(
+        source=parser,  # recurrent connection
+        name='rnn-stack',  # unique identifier
+        fml='stack.focus stack(1).focus',  # look for both stack tokens
+        source_translator='shift-reduce-step',  # maps token indices -> step
+        embedding_dim=64)  # project down to 64 dims
 
-  parser.fill_from_resources(DATA_DIR)
+    parser.fill_from_resources(DATA_DIR)
 
-  master_spec = spec_pb2.MasterSpec()
-  '''
-  master_spec.component.extend(
-      [char2word.spec, lookahead.spec, tagger.spec, parser.spec])
-  '''
-  master_spec.component.extend(
-      [lookahead.spec, tagger.spec, parser.spec])
-  #HTML(render_spec_with_graphviz.master_spec_graph(master_spec))
+    master_spec = spec_pb2.MasterSpec()
+    '''
+    master_spec.component.extend(
+        [char2word.spec, lookahead.spec, tagger.spec, parser.spec])
+    '''
+    master_spec.component.extend(
+        [lookahead.spec, tagger.spec, parser.spec])
+    #HTML(render_spec_with_graphviz.master_spec_graph(master_spec))
   
-  return master_spec
+    return master_spec
 
 def build_graph(master_spec) :
 
@@ -187,7 +187,7 @@ def train(graph, builder, trainer, annotator) :
         sess.run(tf.global_variables_initializer())
         training_corpus = ConllSentenceReader(
             TRAINING_CORPUS_PATH, projectivize=True).corpus()
-        dev_corpus = ConllSentenceReader(DEV_CORPUS_PATH).corpus()[:200]
+        dev_corpus = ConllSentenceReader(DEV_CORPUS_PATH).corpus()
         for step in xrange(N_STEPS):
             trainer_lib.run_training_step(sess, trainer, training_corpus, batch_size=BATCH_SIZE)
             tf.logging.warning('Step %d/%d', step + 1, N_STEPS)

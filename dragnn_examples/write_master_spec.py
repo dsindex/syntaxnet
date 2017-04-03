814168f608d66529e2d7d3a182517d68ba2a0aeb
dsindex <hazzling@gmail.com>
2017-04-03 21:13:03 +0900

    remove spec_file if exists

--- dragnn_examples/write_master_spec.py
+++ dragnn_examples/write_master_spec.py
@@ -4,6 +4,9 @@ import tensorflow as tf
 
 import model_dragnn as model
 
+from tensorflow.python.framework import errors
+from tensorflow.python.platform import gfile
+
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
@@ -12,6 +15,10 @@ flags.DEFINE_string('spec_file', 'parser_spec.textproto',
 
 def main(unused_argv) :
     master_spec = model.build_master_spec()
+    try : 
+        gfile.Remove(FLAGS.spec_file)
+    except errors.OpError as err :
+        tf.logging.error('Unable to delete prior files: %s', err)
     model.write_master_spec(master_spec, FLAGS.spec_file)
 
 if __name__ == '__main__':

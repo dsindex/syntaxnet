a260128e0c95ac88c438da75122e8c89ccad327c
dsindex <hazzling@gmail.com>
2017-04-06 10:28:17 +0900

    for supporting multi-processing mode in tornado server

--- dragnn_examples/master_server.py
+++ dragnn_examples/master_server.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 #-*- coding: utf8 -*-
 import tensorflow as tf
+import model_dragnn as model
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('dragnn_spec', '', 
+                    'Path to the spec defining the model.')
+flags.DEFINE_string('resource_path', '',
+                    'Path to constructed resources.')
+flags.DEFINE_string('checkpoint_filename', '',
+                    'Filename to save the best checkpoint to.')
+flags.DEFINE_bool('enable_tracing', False, 
+                    'Whether tracing annotations')
 
 def main(unused_argv) :
     tf.logging.info('Start master server...')
     server = tf.train.Server.create_local_server()
     print server.target  # for other processes to connect
+    # Loading model
+    m = model.load_model(FLAGS.dragnn_spec,
+                         FLAGS.resource_path,
+                         FLAGS.checkpoint_filename,
+                         enable_tracing=FLAGS.enable_tracing,
+                         tf_master=server.target)
     server.join()
 
 if __name__ == '__main__':
---
+++ master_server.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+# code from http://stackoverflow.com/a/1116890
+function readlink()
+{
+    TARGET_FILE=$2
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+
+    # Iterate down a (possible) chain of symlinks
+    while [ -L "$TARGET_FILE" ]
+    do
+        TARGET_FILE=`readlink $TARGET_FILE`
+        cd `dirname $TARGET_FILE`
+        TARGET_FILE=`basename $TARGET_FILE`
+    done
+
+    # Compute the canonicalized name by finding the physical path
+    # for the directory we're in and appending the target file.
+    PHYS_DIR=`pwd -P`
+    RESULT=$PHYS_DIR/$TARGET_FILE
+    echo $RESULT
+}
+export -f readlink
+
+VERBOSE_MODE=0
+
+function error_handler()
+{
+  local STATUS=${1:-1}
+  [ ${VERBOSE_MODE} == 0 ] && exit ${STATUS}
+  echo "Exits abnormally at line "`caller 0`
+  exit ${STATUS}
+}
+trap "error_handler" ERR
+
+PROGNAME=`basename ${BASH_SOURCE}`
+
+function print_usage_and_exit()
+{
+  set +x
+  local STATUS=$1
+  echo "Usage: ${PROGNAME} [-v] [-v] [-h] [--help]"
+  echo ""
+  echo " Options -"
+  echo "  -v                 enables verbose mode 1"
+  echo "  -v -v              enables verbose mode 2"
+  echo "  -h, --help         shows this help message"
+  exit ${STATUS:-0}
+}
+
+function debug()
+{
+  if [ "$VERBOSE_MODE" != 0 ]; then
+    echo $@
+  fi
+}
+
+GETOPT=`getopt vh $*`
+if [ $? != 0 ] ; then print_usage_and_exit 1; fi
+
+eval set -- "${GETOPT}"
+
+while true
+do case "$1" in
+     -v)            let VERBOSE_MODE+=1; shift;;
+     -h|--help)     print_usage_and_exit 0;;
+     --)            shift; break;;
+     *) echo "Internal error!"; exit 1;;
+   esac
+done
+
+if (( VERBOSE_MODE > 1 )); then
+  set -x
+fi
+
+
+# template area is ended.
+# -----------------------------------------------------------------------------
+if [ ${#} != 0 ]; then print_usage_and_exit 1; fi
+
+# current dir of this script
+CDIR=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]})))
+PDIR=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]}))/..)
+
+# -----------------------------------------------------------------------------
+# functions
+
+function make_calmness()
+{
+	exec 3>&2 # save 2 to 3
+	exec 2> /dev/null
+}
+
+function revert_calmness()
+{
+	exec 2>&3 # restore 2 from previous saved 3(originally 2)
+}
+
+function close_fd()
+{
+	exec 3>&-
+}
+
+function jumpto
+{
+	label=$1
+	cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
+	eval "$cmd"
+	exit
+}
+
+
+# end functions
+# -----------------------------------------------------------------------------
+
+
+
+# -----------------------------------------------------------------------------
+# main 
+
+make_calmness
+if (( VERBOSE_MODE > 1 )); then
+	revert_calmness
+fi
+
+python=/usr/bin/python
+
+DATA_DIR=${CDIR}/dragnn_examples/data
+CHECKPOINT_FILE=${DATA_DIR}/checkpoint.model
+
+function run {
+	cd ${PDIR}
+	${PDIR}/bazel-bin/work/dragnn_examples/master_server \
+                --dragnn_spec=${DATA_DIR}/parser_spec.textproto \
+		--resource_path=${DATA_DIR} \
+		--checkpoint_filename=${CHECKPOINT_FILE}
+}
+
+run
+
+close_fd
+
+# end main
+# -----------------------------------------------------------------------------
